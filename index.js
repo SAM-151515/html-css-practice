6e411f713ea84ee57603496e0d32b794ee687df1
SAM-151515 <85931043+SAM-151515@users.noreply.github.com>
2021-09-11 19:13:25 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const toDoEl = document.getElementById('to-do-el')
 
 
 function add() {
+	let toDo = prompt("Please enter your to-do:", "");
 	toDoEl.innerHTML += `
 		<div class="to-do">
 			<div class="check-box" id="check-el">
 			</div>
-			<h2>Figma Project</h2>
+			<h2>${toDo}</h2>
 		</div>`
 }
