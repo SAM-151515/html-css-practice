e76c4d3412dd633464d58ceebe2fd3c7b6607984
SAM-151515 <85931043+SAM-151515@users.noreply.github.com>
2021-09-12 13:27:28 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const toDoEl = document.getElementById('to-do-el')
+let toDos = []
 
+toDos = JSON.parse(localStorage.getItem("toDos"))
+if(toDos === null) {
+	toDos = []
+}
 
 function add() {
-	let toDo = prompt("Please enter your to-do:", "");
+	const toDo = prompt("Please enter your To-do:", "");
 	toDoEl.innerHTML += `
 		<div class="to-do">
 			<div class="check-box" id="check-el">
 			</div>
 			<h2>${toDo}</h2>
 		</div>`
+
+	toDos.push(toDo)
+	localStorage.setItem("toDos", JSON.stringify(toDos))
 }
+
+function renderListOnRefresh() {
+	const toDos = JSON.parse(localStorage.getItem("toDos"))
+	if(toDos) {
+		for(let todo = 0; todo < toDos.length; todo++) {
+		toDoEl.innerHTML += `
+		<div class="to-do">
+			<div class="check-box" id="check-el">
+			</div>
+			<h2>${toDos[todo]}</h2>
+		</div>`
+		}
+	}	
+}
+
+renderListOnRefresh()
