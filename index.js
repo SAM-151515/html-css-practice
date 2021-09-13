a6b84c69b3474b4a27c35786dad5b0d19a7a6152
SAM-151515 <85931043+SAM-151515@users.noreply.github.com>
2021-09-13 19:35:00 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 const toDoEl = document.getElementById('to-do-el')
-let toDos = []
+let toDoList = []
 
-toDos = JSON.parse(localStorage.getItem("toDos"))
-if(toDos === null) {
-	toDos = []
+toDoList = JSON.parse(localStorage.getItem("toDos"))
+if(toDoList === null) {
+	toDoList = []
 }
 
-function add() {
-	const toDo = prompt("Please enter your To-do:", "");
-	if(toDo == null) {
-		//Do Nothing
-	}
-
-	else {
-		toDoEl.innerHTML += `
-			<div class="to-do">
-				<div class="check-box" id="check-el">
-				</div>
-				<h2>${toDo}</h2>
-			</div>`
-
-		toDos.push(toDo)
-		localStorage.setItem("toDos", JSON.stringify(toDos))
-	}
-}
-
-function renderListOnRefresh() {
-	const toDos = JSON.parse(localStorage.getItem("toDos"))
-	if(toDos) {
-		for(let todo = 0; todo < toDos.length; todo++) {
+function createElement() {
+	const toDoList = JSON.parse(localStorage.getItem("toDos"))
+	toDoEl.innerHTML = null
+	if(toDoList) {
+		for(let todo = 0; todo < toDoList.length; todo++) {
 		toDoEl.innerHTML += `
 		<div class="to-do">
 			<div class="check-box" id="check-el">
 			</div>
-			<h2>${toDos[todo]}</h2>
+			<h2>${toDoList[todo]}</h2>
 		</div>`
 		}
-	}	
+	}
 }
 
-renderListOnRefresh()
+function add() {
+	const toDo = prompt("Please enter your To-do:", "");
+	if(toDo == null || toDo == "") {
+		//Do Nothing
+	}
+	else {
+		toDoList.push(toDo)
+		localStorage.setItem("toDos", JSON.stringify(toDoList))
+		
+		createElement()		
+	}
+}
+
+function del() {
+	localStorage.clear()
+	createElement()
+}
+
+createElement()
